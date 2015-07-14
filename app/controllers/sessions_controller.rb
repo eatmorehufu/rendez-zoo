c2a737e23cef7e06ec7130d205f4616ebf3593ac
Dean <hhu.hufu@gmail.com>
2015-07-14 11:45:45 -0400

    Add create controller action for Sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,23 @@
 class SessionsController < ApplicationController
   def new
-
   end
 
   def create
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+    if user.present?
+      redirect_to groups_url
+    else
+      flash.now[:errors] = "Couldn't find user."
+      render :new
+    end
 
   end
 
   def destroy
 
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
       redirect_to groups_url
     else
       flash.now[:errors] = @user.errors.full_messages
+      render :new
     end
   end
 
