2b20bf8bed2231fe726b96521e24960ff0cb0e2e
Dean <hhu.hufu@gmail.com>
2015-07-14 11:07:20 -0400

    User model stores password in a digest and can look up by password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,33 @@
 class User < ActiveRecord::Base
-  
+  validates :email, :password_digest, :username, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true}
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user.try(:is_password?, password) ? user : nil
+  end
+
+  def password=(password)
+    if password.present?
+      @password = password
+      self.password_digest = BCrypt::Password.create(password)
+    end
+  end
+
+  def is_password(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+
+  end
+
+  private
+
+  def ensure_session_token
+
+  end
+
 end
