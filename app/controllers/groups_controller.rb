65975c647c334af77c2d5f799eb89566ded53ce1
Dean <hhu.hufu@gmail.com>
2015-07-14 14:51:04 -0400

    Groups Index has unordered list of groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,17 @@
 class GroupsController < ApplicationController
   def index
+    @groups = Group.all #eventually this will be based on nearby groups    
   end
 
   def create
-
+    group = Group.new(group_params)
+    if group.save
+      #display some sort of success message
+      redirect_to groups_url
+    else
+      flash.now[:errors] = group.errors.full_messages
+      render :new
+    end
   end
 
   def new
@@ -12,5 +20,9 @@ class GroupsController < ApplicationController
 
   def show
 
+  end
+
+  def group_params
+    params.require(:group).permit(:title, :description, :zip_code)
   end
 end
