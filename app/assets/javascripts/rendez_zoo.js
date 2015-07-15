ce75bba86af16914f7d18d08298a5d1ad3565440
eatmorehufu <hhu.hufu@gmail.com>
2015-07-14 22:16:44 -0400

    Replace vendor assets with backbone gem, auto-generate backbone structure.

---
+++ app/assets/javascripts/rendez_zoo.js
@@ -0,0 +1,10 @@
+window.RendezZoo = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    new RendezZoo.Routers.Router({$rootEl : $("#content")});
+    Backbone.history.start();
+  }
+};
