ed31f48144d082889a02632d260a7d827a552be9
eatmorehufu <hhu.hufu@gmail.com>
2015-07-21 10:36:47 -0400

    Change banner text and background when you've logged in.

--- app/assets/javascripts/views/groups/index.js
+++ app/assets/javascripts/views/groups/index.js
@@ -2,6 +2,8 @@ RendezZoo.Views.GroupsIndex = Backbone.CompositeView.extend({
   template: JST['groups/index'],
   memberTemplate: JST['groups/_member_groups'],
   indexTemplate: JST['groups/_index_groups'],
+  notLoggedInTemplate: JST['groups/_splash_banner'],
+  loggedInTemplate: JST['groups/_user_welcome'],
   tagName: "section",
 
   initialize: function(options) {
@@ -15,12 +17,21 @@ RendezZoo.Views.GroupsIndex = Backbone.CompositeView.extend({
     this.$el.html(content);
 
     if (RendezZoo.currentUser.isSignedIn()) {
+      this.$('.index-banner').removeClass("logged-out");
+      this.$('.banner-content').html(this.loggedInTemplate({
+        currentUser: RendezZoo.currentUser
+      }));
       if (RendezZoo.currentUser.organizerGroups().length > 0) {
         this.$('#group-index-body').append(this.memberTemplate({groups: RendezZoo.currentUser.organizerGroups(), heading: "Your Admin Groups"}));
       }
       if (RendezZoo.currentUser.memberGroups().length > 0) {
         this.$('#group-index-body').append(this.memberTemplate({groups: RendezZoo.currentUser.memberGroups(), heading: "Your Member Groups"}));
       }
+    } else {
+      this.$('.index-banner').addClass("logged-out");
+      this.$('.banner-content').html(this.notLoggedInTemplate({
+        groups: RendezZoo.groups
+      }));
     }
 
     this.$('#group-index-body').append(this.indexTemplate({groups: this.collection}));
