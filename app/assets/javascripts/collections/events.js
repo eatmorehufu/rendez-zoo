664c30d6ec6fcba95e1675f071004f885a1d9d96
eatmorehufu <hhu.hufu@gmail.com>
2015-07-21 15:13:58 -0400

    Add event collection past and upcoming filter.

--- app/assets/javascripts/collections/events.js
+++ app/assets/javascripts/collections/events.js
@@ -1,13 +1,29 @@
 RendezZoo.Collections.Events = Backbone.Collection.extend({
   url: "api/events",
   model: RendezZoo.Models.Event,
+  comparator: "start_time",
 
   pastEvents: function(){
-    
+    if (!this._pastEvents) {
+      this._pastEvents = []
+      this.forEach(function(groupEvent) {
+        if (Date.parse(groupEvent.get('start_time')) > Date.now()){
+          this._pastEvents.push(groupEvent);
+        }
+      }.bind(this))
+    }
+
+    return this._pastEvents
   },
 
   upcomingEvents: function() {
+    var pastLimit = this.pastEvents().length;
+    var upcoming = [];
+    for (var i = pastLimit; i < this.length; i++ ) {
+      upcoming.push(this.at(i));
+    }
 
+    return upcoming;
   }
 
 })
