52ca04ddf4657ecdc633138412a8f83a7754a807
eatmorehufu <hhu.hufu@gmail.com>
2015-07-27 19:02:33 -0400

    Delete poorly sized images, add pencil functionality for user profiles.

--- app/assets/javascripts/views/users/show.js
+++ app/assets/javascripts/views/users/show.js
@@ -3,11 +3,17 @@ RendezZoo.Views.UserShow = Backbone.CompositeView.extend({
   innerTemplate: JST['users/_show'],
   userPicUpload: JST['users/_picForm'],
   usersGroupsListItemTemplate: JST['users/_member_groups_list_item'],
+  pencilButtonTemplate: JST['users/_pencil_button'],
+  usernameLocTemplate: JST['users/_username_loc_input'],
+  bioEditTemplate: JST['users/_bioedit'],
   tagName: "article",
 
   events: {
-    "submit form": "submit",
-    "change #input-user-avatar": "fileInputChange"
+    "submit .userEditForm": "submit",
+    "change #input-user-avatar": "fileInputChange",
+    "click .pencil": "editInPlace",
+    "blur .user-edit": "saveEdit",
+    "submit .user-edit": "saveEdit"
   },
 
   initialize: function(options){
@@ -22,7 +28,7 @@ RendezZoo.Views.UserShow = Backbone.CompositeView.extend({
     this.$('.profile-picture').append(this.userPicUpload());
     this.attachMemberships(this.model.organizerGroups(), '.organizer-memberships');
     this.attachMemberships(this.model.memberGroups(), '.group-memberships');
-
+    this.attachPencils();
     return this;
   },
 
@@ -36,10 +42,51 @@ RendezZoo.Views.UserShow = Backbone.CompositeView.extend({
 
     var that = this;
     this.model.saveFormData(formData, {
-      success: function(){
-        Backbone.history.navigate("", { trigger: true });
-      }
+      success: function(model, resp){
+        RendezZoo.currentUser.set(resp);
+        this.render();
+      }.bind(this)
     });
+  },
+
+  attachPencils: function() {
+    var view = this;
+    this.$('.attach-pencil').each(function(index){
+      var data = $(this).data('field');
+      $(this).append(view.pencilButtonTemplate({ data: data }))
+    })
+  },
+
+  editInPlace: function(event) {
+    event.preventDefault();
+    var formName = $(event.currentTarget).data("field");
+    if (formName === "username" || formName === "zip_code") {
+      this.$('.attach-pencil.' + formName).html(this.usernameLocTemplate({
+        field: formName,
+        value: RendezZoo.currentUser.get(formName)
+      }));
+      this.$('.attach-pencil.' + formName + " input").focus();
+    } else if (formName === "description") {
+      this.$('p.description').html(this.bioEditTemplate({
+        value: RendezZoo.currentUser.get('description')
+      }));
+      this.$('p.description textarea').focus();
+    }
+  },
+
+  saveEdit: function(event){
+    event.preventDefault();
+    var attrs = $(event.currentTarget).serializeJSON()
+    $.ajax({
+      url: "/api/users/" + RendezZoo.currentUser.id,
+      method: "PUT",
+      data: attrs,
+      success: function(model) {
+        RendezZoo.currentUser.set(model);
+        this.model.set(model);
+        this.render();
+      }.bind(this)
+    })
   }
 
 })
