8054fa2af34373e6ae60b46429e76f94b69a927e
eatmorehufu <hhu.hufu@gmail.com>
2015-07-27 21:28:10 -0400

    re-write seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+valid_zips = [
+  "11375",
+  "11101",
+  "10003",
+  "12345",
+  "11580",
+  "11364",
+  "11234",
+  "07032",
+  "07031",
+  "07079",
+  "07083",
+  "10457",
+  "10461",
+  "11554",
+  "11530",
+  "11756"
+]
 
-def add_past_event(group, title, desc, loc_name, city, state)
-  group.events.create!(
-    title: title,
-    description: desc,
-    loc_name: loc_name,
-    city: city,
-    state: state,
-    start_time: rand(5).weeks.ago
-  )
-end
-
-def add_future_event(group, title, desc, loc_name, city, state)
-  group.events.create!(
-    title: title,
-    description: desc,
-    loc_name: loc_name,
-    city: city,
-    state: state,
-    start_time: rand(5).weeks.from_now
-  )
-end
+valid_streets = [
+  "Park Ave",
+  "Madison Ave",
+  "Lexington Ave",
+  "5th Ave",
+  "1st Ave",
+  "2nd Ave",
+  "8th Ave",
+  "Broadway"
+]
 
 ["bears", "predators", "prey", "birds", "mammals", "sealife", "furry",
-"otters", "cats", "giraffes", "basketball"].each do |category|
+"otters", "cats", "giraffes", "fish", "mollusks", "migration", "hibernation",
+"mating", "nesting", "rearing young", "salt lick", "scavenger", "symbiosis",
+"budding", "reptiles", "tundra", "plains", "forest", "jungle", "ecosystem",
+"food chain"].each do |category|
   Category.create!(name: category);
 end
-
-
-
 
 sennacy = User.create!(
   username: 'Sennacy',
@@ -43,35 +43,77 @@ sennacy = User.create!(
   zip_code: "10003",
   avatar: "http://i.imgur.com/kjH1unY.jpg"
 )
-5.times do |i|
-  UserInterest.create!(user_id: sennacy.id, category_id: i + 1)
+3.times do
+  UserInterest.create!(user_id: sennacy.id, category_id: Category.ids.sample)
 end
 
-["Bear", "Polar Bear", "Otter", "Dolphin", "Giraffe", "Hyena", "Shark", "Meerkat", "Flamingo"].each do |name|
-  user = User.create!(username: name, email: name + "@" + name + "." + name, password: 'password', zip_code: (rand(2000) + 10000).to_s)
-  sleep 0.3
+50.times do |i|
+  animal = Faker::Team.creature.capitalize
+  user = User.create!(
+    username: Faker::Name.first_name + " " + animal,
+    email: animal + i.to_s + "@" + animal + "." + animal,
+    password: "password",
+    zip_code: valid_zips.sample,
+    description: "Hello! New to this site, I really love " + Faker::Team.creature + ".
+    I'm originally from " + Faker::Address.country + " but I've migrated here
+    for the summer.",
+    avatar: File.new(File.join(Rails.root, "public", "images", "#{rand(18) + 1}.jpg"))
+  )
   3.times do
-    group = user.owned_groups.create!(
-      title: Faker::Team.name + " of the " + Faker::Address.city_suffix,
-      description: Faker::Lorem.paragraph(3),
-      zip_code: user.zip_code,
-      avatar: File.new(File.join(Rails.root, "public", "images", "#{rand(13) + 1}.jpg"))
-    )
-    sleep 0.2
-    GroupMembership.create!(group_id: group.id, member_id: user.id, status: "organizer")
-    GroupCategory.create!(group_id: group.id, category_id: rand(10) + 1)
+    UserInterest.create!(user_id: user.id, category_id: Category.ids.sample)
+  end
+  sleep 0.3
+end
+
+50.times do
+  animal = Faker::Team.name
+  user = User.find(User.ids.sample)
+  group = user.owned_groups.create!(
+    title: animal + " of the " + Faker::Address.city_suffix.capitalize,
+    description: Faker::Lorem.paragraphs(3),
+    zip_code: valid_zips.sample,
+    avatar: File.new(File.join(Rails.root, "public", "images", "#{rand(18) + 1}.jpg")),
+    flair: animal
+  )
+  GroupMembership.create!(group_id: group.id, member_id: user.id, status: "organizer")
+
+  3.times do
+    GroupCategory.create!(group_id: group.id, category_id: Category.ids.sample)
   end
 end
 
-user = User.find_by(username: "Bear")
-group = user.owned_groups.create!(
+group = sennacy.owned_groups.create!(
   title: "Urban Bears",
-  description: "Come hang out with the bears in the city. Fun fur all.",
-  zip_code: "10002"
+  description: "Come hang out with the bears in the city. Fun fur all, and all fur fun.
+  Our mission is to grow our community, attract tourism, and spotlight the individuals who make this forest great, while offering fun opportunities to connect socially, animalistically and more.",
+  zip_code: "10002",
+  flair: "bears"
 )
-GroupMembership.create!(group_id: group.id, member_id: user.id, status: "organizer")
+GroupMembership.create!(group_id: group.id, member_id: sennacy.id, status: "organizer")
 sleep 0.2
-group = user.owned_groups.create!(
+
+5.times do
+  event = group.events.create!(
+    start_time: rand(5).weeks.from_now,
+    title: Faker::App.author + "'s " + Faker::App.name + "-a-thon!",
+    description: Faker::Company.suffix + " of " + Faker::Team.creature + "
+    enjoying the sun and fair weather. Come one, come all!",
+    street1: ((rand(5) * 100) + (rand(5) * 10)).to_s + " " + valid_streets.sample,
+    city: "New York",
+    state: "NY"
+  )
+  10.times do
+    attendant = User.ids.sample
+    until !EventAttendance.find_by(event_id: event.id, attendant_id: attendant.id)
+      attendant = User.ids.sample
+    end
+    EventAttendance.create!(event_id: event.id, attendant_id: attendant.id)
+  end
+end
+
+
+
+group = sennacy.owned_groups.create!(
   title: "Polar Bear Club",
   description: "Cold enough for ya?
 
@@ -86,65 +128,31 @@ NEW YEAR’S DAY: Assemble on the Boardwalk at Stillwell Avenue, Coney Island.
 SWIM TIME 1:00 PM SHARP, arrive early.
 
 FREE ADMISSION TO NY HUMAN PENITENTIARY TO ALL REGISTERED ATTENDEES",
-  zip_code: "11224"
+  zip_code: "11224",
+  flair: "polar bears"
 )
-GroupMembership.create!(group_id: group.id, member_id: user.id, status: "organizer")
-sleep 0.2
-event = add_past_event(group,
-  "Fur Ball",
-  "The Furball is a NYC based danceparty & celebration of everything furry & fuzzy.",
-  "Central Park",
-  "New York",
-  "NY"
-)
-
-event.attendee_ids += [user.id]
-
-event = add_past_event(group,
-  "Going to the Maul",
-  "Sometimes, humans are the prey animals.",
-  "Central Park",
-  "New York",
-  "NY"
-)
-
-event.attendee_ids += [user.id]
+GroupMembership.create!(group_id: group.id, member_id: sennacy.id, status: "organizer")
 
 sleep 0.2
-user = User.find_by(username: "Otter")
-group = user.owned_groups.create!(
-  title: "Significant Otters of LI",
-  description: "Welcome to The LI 20's, 30's and 40's Singles Group. We are a fun group of young otters from the bay area who come together to meet and be merry, as well as make new friendships.
+group = sennacy.owned_groups.create!(
+  title: "Significant Otters of LIC",
+  description: "Welcome to The LIC 20's, 30's and 40's Singles Group. We are a fun group of young otters from the bay area who come together to meet and be merry, as well as make new friendships.
 
 Our diverse events include: happy hour sea urchins, going to some of the hottest beaches as well as pubs/bars on the island, themed parties, sporting/recreational events, speed dating, and more!
 
 We aim to be a caring and compassionate group where people will be respectful of each other despite different species, interests, values, and beliefs.  Come join us, and broaden your social horizon, as we seek to be the bridge to bring people together.",
-  zip_code: "94110"
+  zip_code: "11101"
 )
 
 GroupMembership.create!(group_id: group.id, member_id: user.id, status: "organizer")
-sleep 0.2
-event = add_future_event(group,
-  "Splish Splash",
-  "Bid bon voyage to the bar scene for a night and experience a new social experience on the river!
-
-Join 200+ singles & swim around the bay for a 4 hour tour & party! Mix, mingle & dance the night away.",
-  "Golden Gate Bridge",
-  "San Francisco",
-  "CA"
-)
-
-event.attendee_ids += [user.id]
-
-20.times do
-  user = User.create!(
-    username: Faker::Name.name,
-    email: Faker::Internet.email,
-    password: "password",
-    zip_code: (11101 + rand(200)).to_s,
-    avatar: File.new(File.join(Rails.root, "public", "images", "#{rand(13) + 1}.jpg"))
-  )
-  sleep 0.3
-  #
-  GroupMembership.create!(group_id: group.id, member_id: user.id)
-end
+# event = add_future_event(group,
+#   "Splish Splash",
+#   "Bid bon voyage to the bar scene for a night and experience a new social experience on the river!
+#
+# Join 200+ singles & swim around the bay for a 4 hour tour & party! Mix, mingle & dance the night away.",
+#   "Golden Gate Bridge",
+#   "San Francisco",
+#   "CA"
+# )
+#
+# event.attendee_ids += [user.id]
